aef10839d268d24e236652b82512b4a6a1c64f71
Vani <workwithvanichaturvedi@gmail.com>
2024-04-14 11:42:48 +0530

    Completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,23 +1,53 @@
 const { where } = require('sequelize');
 const {City} = require('../models/index');
 class CityRepository{
-    async createCity({ name }){
+
+    async createCity({ name }){ // {name: "New Delhi"}
         try{
-            const city = await City.create({ name });
+            const city= await City.create({
+                name: name
+            });
             return city;
-        }catch(error){
+        } catch(error){
+            console.log("Something went wrong in the repositroy layer");
             throw {error};
         }
     }
 
-    async deleteCity({ cityId }){
-        try{
+    async deleteCity(){
+        try {
             await City.destroy({
                 where: {
                     id: cityId
                 }
             });
+            return true;
+        }catch(error){
+            console.log("Something went wrong in the repositroy layer");
+            throw {error};
+        }
+    }
+
+    async updateCity(cityId, data){
+        try{
+            const city = await City.update(data, {
+                where: {
+                    id: cityID
+                }
+            });
+            return city;
+        }catch(error){
+            console.log("Something went wrong in the repositroy layer");
+            throw {error};
+        }
+    }
+
+    async getCity(){
+        try{
+            const city = await City.findByPk(cityId);
+            return city;
         } catch(error){
+            console.log("Something went wrong in the repositroy layer");
             throw {error};
         }
     }
