5ba62cd5a80c07262885235cf5357e7c86ebd3a6
Vani <workwithvanichaturvedi@gmail.com>
2024-04-20 11:43:16 +0530

    Added create flights CRUD

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,7 @@
 const express = require ('express');
 const CityController = require('../../controllers/city-controller');
 const AirportController = require('../../controllers/airport-controller');
+const FlightController = require('../../controllers/flight-controller');
 
 const router = express.Router();
 
@@ -10,10 +11,11 @@ router.get('/city/:id',CityController.get);
 router.get('/city',CityController.getAll);
 router.patch('/city/:id',CityController.update);
 
-router.post('/airport',AirportController.create); //done
-router.delete('/airport/:id',AirportController.destroy); //done
-router.get('/airport/:id',AirportController.get); //done
-router.get('/airport',AirportController.getAll);  //done
-router.patch('/airport/:id',AirportController.update); //done
+router.post('/airports',AirportController.create); //done
+router.delete('/airports/:id',AirportController.destroy); //done
+router.get('/airports/:id',AirportController.get); //done
+router.get('/airports',AirportController.getAll);  //done
+router.patch('/airports/:id',AirportController.update); //done
 
+router.post('/flights',FlightController.create);
 module.exports = router;
